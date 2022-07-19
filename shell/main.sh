33e58e2af1d42cbd0ef77189eadf5bae3564dba5
Mikas Pupeikis <mikas.pupeikis@mif.stud.vu.lt>
2022-07-19 22:04:51 +0300

    Add ansible files

---
+++ shell/main.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+CUSER=CUSER
+CPASS=CPASS
+CENDPOINT=https://grid5.mif.vu.lt/cloud3/RPC2
+
+VM_TEMPLATE_STRING=VM_TEMPLATE_STRING
+VM_NAME=VM_NAME
+
+CVMREZ=$(onetemplate instantiate $VM_TEMPLATE_STRING --name $VM_NAME  --user $CUSER --password $CPASS --endpoint $CENDPOINT)
+CVMID=$(echo $CVMREZ |cut -d ' ' -f 3) 
+echo $CVMID
+echo "Waiting for VM to RUN 30 sec."
+sleep 30
+
+$(onevm show $CVMID --user $CUSER --password $CPASS  --endpoint $CENDPOINT >$CVMID.txt)
+CSSH_CON=$(cat $CVMID.txt | grep CONNECT\_INFO1| cut -d '=' -f 2 | tr -d '"'|sed 's/'$CUSER'/root/')
+CSSH_PRIP=$(cat $CVMID.txt | grep PRIVATE\_IP| cut -d '=' -f 2 | tr -d '"')
+
+echo "Connection string: $CSSH_CON"
+echo "Local IP: $CSSH_PRIP"
+
+SHOW_VMS=$(onevm list --user $CUSER --password $CPASS --endpoint $CENDPOINT)
+echo "$SHOW_VMS"
+exit 0
--- src/README.md
+++ src/README.md
@@ -7,8 +7,9 @@
 2. Install node dependencies `npm i`
 3. Restore `vendor/` folder `composer update`
 4. Create `.env` file, contents should be based on `.env.example`
-5. Update the key stored in the application's environment file `php artisan key:generate`
-6. Serve laravel app `php artisan serve`
+5. Create hard links of assets in the storage/ folder `php artisan storage:link`
+6. Update the key stored in the application's environment file `php artisan key:generate`
+7. Serve laravel app `php artisan serve`
 
 ## Development tips
 1. Developing with HMR enabled `npm run watch`
