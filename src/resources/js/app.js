266d5571dbe30d00bbbf09edd7e46f2db7ed356e
Mikas Pupeikis <mikas.pupeikis@stud.mif.vu.lt>
2022-08-17 05:20:25 +0300

    remove current vue logic

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,14 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import { createApp } from 'vue';
-
-import AppLayout from './layouts/AppLayout.vue';
-import { Plugins } from './plugins';
-import { Router } from './router';
 
 window.Alpine = Alpine;
 
 Alpine.start();
-
-createApp(AppLayout).use(Router).use(Plugins).mount('#app');
