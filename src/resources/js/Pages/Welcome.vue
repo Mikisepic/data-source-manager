02509e2cc0be72aaed5dc86e619a540a2c933a1f
Mikas Pupeikis <mikas.pup@gmail.com>
2022-08-22 23:16:48 +0300

    Different display for welcome page

--- src/resources/js/Pages/Welcome.vue
+++ src/resources/js/Pages/Welcome.vue
@@ -39,36 +39,19 @@ defineProps({
         </template>
       </div>
 
-      <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-          <img
-            class="h-20 w-auto text-gray-700 sm:h-40"
-            src="/storage/img/logo.png"
-          />
-        </div>
-
+      <div class="container">
         <div
           class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg"
         >
           <div class="grid grid-cols-1 md:grid-cols-2">
-            <div class="p-6">
-              <div class="ml-12"></div>
+            <div>
+              <img src="/storage/img/illustration.jpg" />
             </div>
 
             <div
-              class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l"
+              class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l flex justify-center items-center"
             >
-              <div class="ml-12"></div>
-            </div>
-
-            <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-              <div class="ml-12"></div>
-            </div>
-
-            <div
-              class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l"
-            >
-              <div class="ml-12"></div>
+              <img class="w-auto text-gray-700" src="/storage/img/logo.png" />
             </div>
           </div>
         </div>
--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -5,13 +5,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title inertia>{{ config('app.name', 'Laravel') }}</title>
+  <title inertia>{{ config('app.name', 'MAGicle') }}</title>
 
   <!-- Icon -->
   <link rel="icon" href="{{ url('storage/img/icon.png') }}">
-
-  <!-- Fonts -->
-  <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
   <!-- Scripts -->
   @routes
