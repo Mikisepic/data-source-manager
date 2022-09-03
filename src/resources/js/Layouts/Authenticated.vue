41ca931361cc24aa0400f6baff163b3c5581823e
Mikas Pupeikis <mikas.pup@gmail.com>
2022-09-03 20:15:59 +0300

    Fix empty pagination and content issue

--- src/resources/js/Layouts/Authenticated.vue
+++ src/resources/js/Layouts/Authenticated.vue
@@ -151,7 +151,7 @@ const items = [
       </header>
 
       <main>
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl min-h-[74vh] mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden sm:rounded-lg">
             <div class="bg-white rounded-lg py-4 px-4 dark:bg-gray-800">
               <slot />
@@ -159,6 +159,28 @@ const items = [
           </div>
         </div>
       </main>
+
+      <footer
+        class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
+      >
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {{ new Date().getFullYear() }}
+          <a href="#" class="hover:underline">MAGicle™</a>. All Rights Reserved.
+        </span>
+        <ul
+          class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+        >
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Contributing</a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline">Contact</a>
+          </li>
+        </ul>
+      </footer>
     </div>
   </div>
 </template>
